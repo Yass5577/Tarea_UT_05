1eb5acbf3fb1f5780ebac90e18717501c039b240
Windows7 <Windows7@192.168.1.88>
2016-06-13 18:43:05 +0100

    Añadir una nueva variable de tipo float y nombre cantidad al metodo operativa_cuenta

--- src/dam_ed04_actividad/Main.java
+++ src/dam_ed04_actividad/Main.java
@@ -16,6 +16,7 @@ public class Main {
     private static void operativa_cuenta(){
         CCuenta cuenta1;
         double saldoActual;
+        float cantidad;
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
