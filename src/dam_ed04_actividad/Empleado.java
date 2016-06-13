2926be9cf91f0c657a280590ce1533cee6927a4a
Windows7 <Windows7@192.168.1.88>
2016-06-13 18:55:17 +0100

    Creacion de la clase Empleado y sus Constructores

---
+++ src/dam_ed04_actividad/Empleado.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author Windows7
+ */
+public class Empleado {
+    String nombre;
+    String apellidos;
+    String dni;
+
+    public Empleado() {
+        nombre = null;
+        apellidos = null;
+        dni = null;
+    }
+
+    public Empleado(String nombre, String apellidos, String dni) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+    }
+    
+    
+}
