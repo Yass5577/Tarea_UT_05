8892b16272f0190120ca84be0a001123c0be04f3
Windows7 <Windows7@192.168.1.88>
2016-06-13 19:08:53 +0100

    Creacion de la clase Banquero y sus constructores

---
+++ src/dam_ed04_actividad/Banquero.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author Windows7
+ */
+public class Banquero {
+    String nombre;
+    String apellidos;
+    String dni;
+
+    public Banquero() {
+        nombre = null;
+        apellidos = null;
+        dni = null;
+    }
+
+    public Banquero(String nombre, String apellidos, String dni) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+    }
+    
+    
+}
